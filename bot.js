fca3fba4747ddbccc6a0a2ac3b42b5ee8474a517
Nathan Balli <nathanballi@Nathans-iMac.local>
2017-03-03 12:17:02 -0500

    added truth

--- bot.js
+++ bot.js
@@ -8,11 +8,14 @@ function respond() {
       botRegex = /^\/cool$/,
       botTruth   = /^\/truth$/;
   
-  if(request.text && botRegex.test(request.text)) {
+  console.log('NICOLE NASSIF = ' + request.user_id);
+  console.lot('should be     = 20120116');
+  
+  if(request.text && botRegex.test(request.text)) { // Send face
     this.res.writeHead(200);
     postMessage(1);
     this.res.end();
-  } else if(request.text && botTruth.test(request.text)) {
+  } else if(request.text && botTruth.test(request.text)) { // Michigan sucks
   	this.res.writeHead(200);
   	postMessage(2);
   	this.res.end();
@@ -62,41 +65,5 @@ function postMessage(type) {
   });
   botReq.end(JSON.stringify(body)); // THIS LINE SENDS MESSAGE TO GROUP CHAT
 }
-
-function postTruthMessage() {
-  var botResponse, options, body, botReq;
-
-  botResponse = '*ichigan sucks';
-
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST'
-  };
-
-  body = {
-    "bot_id" : botID,
-    "text" : botResponse
-  };
-
-  console.log('sending ' + botResponse + ' to ' + botID);
-
-  botReq = HTTPS.request(options, function(res) {
-      if(res.statusCode == 202) {
-        //neat
-      } else {
-        console.log('rejecting bad status code ' + res.statusCode);
-      }
-  });
-
-  botReq.on('error', function(err) {
-    console.log('error posting message '  + JSON.stringify(err));
-  });
-  botReq.on('timeout', function(err) {
-    console.log('timeout posting message '  + JSON.stringify(err));
-  });
-  botReq.end(JSON.stringify(body));
-}
-
 
 exports.respond = respond;
